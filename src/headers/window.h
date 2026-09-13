22a0a9099d3d356d981e4bc06b5162a210755860
rarfel <faelmvitor@gmail.com>
2026-09-13 21:48:14 +0000

    crude fix of repel and attract, needs optimization and refactoring in the functions

--- src/headers/window.h
+++ src/headers/window.h
@@ -6,36 +6,33 @@ struct SDLWindowState
 {
   SDL_Window *window;
   SDL_Renderer *renderer;
-  float width;
-  float height;
+  float width = -1;
+  float height = -1;
 };
 
 struct Particle
 {
-  SDL_FRect particle;
-  float speedX;
-  float speedY;
+  SDL_FRect particle = {-1,-1,-1,-1};
+  float speedX = 0;
+  float speedY = 0;
   bool active = false;
-  bool collided;
-  glm::ivec3 color;
+  glm::ivec3 color = {0,0,0};
 };
 
 int InitSDL();
 
 int CreateWindowAndRenderer(const char *title, SDL_WindowFlags flags, SDLWindowState &state);
+bool EventHandler(SDLWindowState *state, SDL_Event &event, bool &restart);
+void DrawBackground(SDLWindowState state, glm::vec4 backgroundColor);
+void DrawBackground(SDLWindowState state, glm::vec4 backgroundColor);
+void CleanUp(SDLWindowState &state);
 
 void LoopHandler(SDLWindowState state, glm::vec4 backgroundColor, Particle particles[], size_t sizeParticlues);
 
-bool EventHandler(SDLWindowState *state, SDL_Event &event);
-
-void CheckCollision(SDLWindowState state, Particle &particles, float speed);
-
 void InitParticles(SDLWindowState state, Particle &particles);
+void RestartPartilcles(SDLWindowState state, Particle &particles);
+void WallCollision(SDLWindowState state, Particle particles[], int i);
 
-void MoveParticles(SDLWindowState state, Particle particles[], size_t sizeParticlues, float speed, float deltaTime);
+void Attract(Particle particles[], size_t sizeParticlues, float vel, float deltaTime, int i, int j);
 
-void DrawBackground(SDLWindowState state, glm::vec4 backgroundColor);
-
-void DrawBackground(SDLWindowState state, glm::vec4 backgroundColor);
-
-void CleanUp(SDLWindowState &state);
+void MoveParticles(SDLWindowState state, Particle particles[], size_t sizeParticlues, float speed, float deltaTime, bool &restart);
--- src/window.cpp
+++ src/window.cpp
@@ -1,5 +1,4 @@
 #include "headers/window.h"
-#include <cmath>
 #include <cstddef>
 #include <cstdlib>
 #include <ctime>
@@ -29,6 +28,7 @@ int CreateWindowAndRenderer(const char *title, SDL_WindowFlags flags, SDLWindowS
 void LoopHandler(SDLWindowState state, glm::vec4 backgroundColor, Particle particles[], size_t sizeParticlues)
 {
   bool running = true;
+  bool restart = false;
   uint64_t previousTime = SDL_GetTicks();
   const float PI = 3.1415;
   float speed = 0;
@@ -42,11 +42,11 @@ void LoopHandler(SDLWindowState state, glm::vec4 backgroundColor, Particle parti
 
     while (SDL_PollEvent(&event))
     {
-      running = EventHandler(&state, event);
+      running = EventHandler(&state, event, restart);
     }
     DrawBackground(state, backgroundColor);
 
-    MoveParticles(state, particles, sizeParticlues, speed, deltaTime);
+    MoveParticles(state, particles, sizeParticlues, speed, deltaTime, restart);
 
     //swap buffers and show to screen
     SDL_RenderPresent(state.renderer);
@@ -56,38 +56,32 @@ void LoopHandler(SDLWindowState state, glm::vec4 backgroundColor, Particle parti
   CleanUp(state);
 }
 
-void Attract(Particle particles[], size_t sizeParticlues, float vel, float deltaTime, int i)
+void Repel(Particle particles[], size_t sizeParticlues, float deltaTime, int i, int j)
 {
-  float xAxis, yAxis;
-  float repel = 1; // how much particles repel each other, setting to zero will make the last particle immobile and every particle attract to it
-  for (int j = i + 1; j < sizeParticlues; j++)
+  float xAxis;
+  float yAxis;
+  float force = 0.1;
+
+  xAxis = particles[j].particle.x - particles[i].particle.x;
+  yAxis = particles[j].particle.y - particles[i].particle.y;
+
+  // this repels from first to last - 1
+  particles[i].particle.x += ((-xAxis) * particles[i].speedX * force) * deltaTime;
+  particles[i].particle.y += ((-yAxis) * particles[i].speedY * force) * deltaTime;
+
+  // this repels the last
+  if(j >= sizeParticlues - 1)
   {
-    if(particles[j].active)
-    {
-      particles[i].speedX = vel;
-      particles[i].speedY = vel;
-
-      particles[j].speedX = vel;
-      particles[j].speedY = vel;
-
-      xAxis = particles[j].particle.x - particles[i].particle.x;
-      yAxis = particles[j].particle.y - particles[i].particle.y;
-      float distance = sqrt((std::pow(xAxis,2) + std::pow(yAxis,2)));
-
-      particles[i].particle.x += ((xAxis) * particles[i].speedX) * deltaTime;
-      particles[i].particle.y += ((yAxis) * particles[i].speedY) * deltaTime;
-
-      //particles[j].particle.x -= ((xAxis) * particles[j].speedX) * deltaTime;
-      //particles[j].particle.y -= ((yAxis) * particles[j].speedY) * deltaTime;
-
-      particles[sizeParticlues-i].particle.x += ((-xAxis) * particles[sizeParticlues-i].speedX * repel) * deltaTime;
-      particles[sizeParticlues-i].particle.y += ((-yAxis) * particles[sizeParticlues-i].speedY * repel) * deltaTime;
-      particles[sizeParticlues-1].color = {0,0,255}; // marking the last particle
-    }
+    particles[j].particle.x += ((xAxis) * particles[j].speedX * force) * deltaTime;
+    particles[j].particle.y += ((yAxis) * particles[j].speedY * force) * deltaTime;
   }
+
+  particles[0].color = {0,255,0}; // marking the first particle
+  particles[sizeParticlues-1].color = {0,0,255}; // marking the last particle
 }
 
-void MoveParticles(SDLWindowState state, Particle particles[], size_t sizeParticlues, float speed, float deltaTime)
+
+void MoveParticles(SDLWindowState state, Particle particles[], size_t sizeParticlues, float speed, float deltaTime, bool &restart)
 {
   for (int i = 0; i < sizeParticlues; i++)
   {
@@ -97,18 +91,75 @@ void MoveParticles(SDLWindowState state, Particle particles[], size_t sizePartic
       SDL_SetRenderDrawColor(state.renderer, particles[i].color.r, particles[i].color.g, particles[i].color.b, 255);
       SDL_RenderFillRect(state.renderer, &particles[i].particle);
 
-      Attract(particles, sizeParticlues, 0.1, deltaTime, i);
+      for (int j = i + 1; j < sizeParticlues; j++)
+      {
+        if(particles[j].active)
+        {
+          Attract(particles, sizeParticlues, 0.1, deltaTime, i, j);
+          Repel(particles, sizeParticlues, deltaTime, i, j);
+        }
+      }
+      WallCollision(state, particles, i);
+    }
+    if (restart)
+    {
+      RestartPartilcles(state,particles[i]);
     }
   }
+
+  restart = false;
+}
+
+void WallCollision(SDLWindowState state, Particle particles[], int i)
+{
+  if(particles[i].particle.x > state.width)
+  {particles[i].particle.x = -particles[i].particle.w;}
+
+  if(particles[i].particle.x < -particles[i].particle.w)
+  {particles[i].particle.x = state.width;}
+
+  if(particles[i].particle.y > state.height)
+  {particles[i].particle.y = -particles[i].particle.h;}
+
+  if(particles[i].particle.y < -particles[i].particle.h)
+  {particles[i].particle.y = state.height;}
+}
+
+void Attract(Particle particles[], size_t sizeParticlues, float vel, float deltaTime, int i, int j)
+{
+  float xAxis;
+  float yAxis;
+
+  particles[i].speedX = vel;
+  particles[i].speedY = vel;
+
+  particles[j].speedX = vel;
+  particles[j].speedY = vel;
+
+  xAxis = particles[j].particle.x - particles[i].particle.x;
+  yAxis = particles[j].particle.y - particles[i].particle.y;
+
+  // this attracts from first to last - 1
+  particles[i].particle.x += ((xAxis) * particles[i].speedX) * deltaTime;
+  particles[i].particle.y += ((yAxis) * particles[i].speedY) * deltaTime;
+
+  // this attracts the last
+  if(j >= sizeParticlues - 1)
+  {
+    particles[j].particle.x += ((-xAxis) * particles[j].speedX) * deltaTime;
+    particles[j].particle.y += ((-yAxis) * particles[j].speedY) * deltaTime;
+  }
+
+  particles[0].color = {0,255,0}; // marking the first particle
+  particles[sizeParticlues-1].color = {0,0,255}; // marking the last particle
 }
 
 void InitParticles(SDLWindowState state, Particle &particles)
 {
-  int size = 5;
+  int size = 20;
   if (!particles.active)
   {
     particles.active = true;
-    particles.collided = false;
     particles.particle.w = size;
     particles.particle.h = size;
 
@@ -118,7 +169,19 @@ void InitParticles(SDLWindowState state, Particle &particles)
   }
 }
 
-bool EventHandler(SDLWindowState *state, SDL_Event &event)
+void RestartPartilcles(SDLWindowState state, Particle &particles)
+{
+  if (particles.active)
+  {
+    particles.active = false;
+
+    particles.particle.x = rand() % (int)(state.width * 0.75);
+    particles.particle.y = rand() % (int)(state.height * 1.1);
+    particles.color = {255, 0, 0};
+  }
+}
+
+bool EventHandler(SDLWindowState *state, SDL_Event &event, bool &restart)
 {
   switch (event.type)
   {
@@ -133,6 +196,13 @@ bool EventHandler(SDLWindowState *state, SDL_Event &event)
       state->height = event.window.data2;
       break;
     }
+    case SDL_EVENT_KEY_UP:
+      if (event.key.key == SDLK_R)
+      {
+        SDL_Log("Restarting...");
+        restart = true;
+      }
+      break;
   }
   return true;
 }
